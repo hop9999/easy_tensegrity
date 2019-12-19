1a0928cd6d448fbd6b4c1b954b76c1e4ddf0a39d
SergeiSa <sergey90mtkgtu@mail.ru>
2019-12-19 17:17:11 +0300

    fixed stiffness errors

--- analitic_stiffness.m
+++ analitic_stiffness.m
@@ -10,9 +10,9 @@ k  = robot.k;
 % Stiffness = k(1)/(norm(r)^3) * ...
 % ( norm(r) * (r*r') + (norm(r) - ro(1)) * (norm(r)^2 *eye(2) - r*r'));
 
-S1 = getStiffness(r, k(1), ro(1));
-S2 = getStiffness(r, k(2), ro(2));
-S3 = getStiffness(r, k(3), ro(3));
+S1 = getStiffness(r, robot.r1, k(1), ro(1));
+S2 = getStiffness(r, robot.r2, k(2), ro(2));
+S3 = getStiffness(r, robot.r3, k(3), ro(3));
 
 S = S1 + S2 + S3;
 S = simplify(S);
@@ -34,9 +34,12 @@ matlabFunction(tensor_K_r,  'File', 'g_tensor_K_r',   'Vars', {r, ro});
 matlabFunction(tensor_K_ro, 'File', 'g_tensor_K_ro',  'Vars', {r, ro});
 matlabFunction(vS_0,        'File', 'g_vS_0',         'Vars', {r, ro});
 
-function S = getStiffness(r, k, ro)
-S = -k/(norm(r)^3) * ...
-( norm(r) * (r*r') + (norm(r) - ro) * (norm(r)^2 *eye(2) - r*r'));
+function S = getStiffness(r, r1, k, ro)
+
+x = r1 - r;
+
+S = k/(norm(x)^3) * ...
+( norm(x) * (x*x') + (norm(x) - ro) * (norm(x)^2 *eye(2) - x*x') );
 S = simplify(S);
 end
 
--- get_robot.m
+++ get_robot.m
@@ -2,8 +2,8 @@ function robot = get_robot()
 
 robot.m = 1;
 
-robot.ro = [0.1,0.1,0.1];
-robot.k = [500,500,500];
+robot.ro = [0.1; 0.1; 0.1];
+robot.k = [10,10,10];
 robot.b = eye(2);
 
 robot.r1 = [sqrt(3)/2; 1/2];
